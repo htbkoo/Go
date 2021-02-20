016633ee899400e155757b8bd0b47f3de9f9bd78
htbkoo <h1017760@connect.hku.hk>
2021-02-20 14:10:18 +0800

    codewars - 8kyu - solved ThirdAngleOfATriangle.go

---
+++ codewars/kyu8/kata/ThirdAngleOfATriangle.go
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/5a023c426975981341000014/train/go
+
+// You are given two interior angles (in degrees) of a triangle.
+//
+// Write a function to return the 3rd.
+//
+// Note: only positive integers will be tested.
+//
+// https://en.wikipedia.org/wiki/Triangle
+
+package kata
+
+func OtherAngle(a int, b int) int {
+	return 180 - a - b
+}
