7d914db69300b9d0f7fc1679e889b0f002e10eae
htbkoo <h1017760@connect.hku.hk>
2021-02-20 13:34:21 +0800

    codewars - 7kyu - added description in FunctionalAddition.go

--- codewars/kyu7/kata/FunctionalAddition.go
+++ codewars/kyu7/kata/FunctionalAddition.go
@@ -1,5 +1,14 @@
 package kata
 
+// https://www.codewars.com/kata/538835ae443aae6e03000547/train/go
+
+// Create a function add(n)/Add(n) which returns a function that always adds n to any number
+//
+// Note for Java: the return type and methods have not been provided to make it a bit more challenging.
+//
+// var addOne = Add(1)
+// addOne(3) // 4
+
 func Add(a int) func(int) int {
 	return func(b int) int {
 		return a + b
