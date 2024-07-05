b95670b8524caf27036e38e59f81bf67961167f2
htbkoo <htbkoo+github@proton.me>
2024-07-05 03:34:50 +0200

    feat(TourOfGo): Update `learn-go/Tour of Go/`

--- learn-go/Tour of Go/More types  structs, slices, and maps/Exercise  Slices/task.go
+++ learn-go/Tour of Go/More types  structs, slices, and maps/Exercise  Slices/task.go
@@ -1,9 +1,58 @@
 package main
 
-import "golang.org/x/tour/pic"
+import (
+	"golang.org/x/tour/pic"
+)
 
 func Pic(dx, dy int) [][]uint8 {
 	/* TODO */
+
+	//p := make([][]uint8, dy)
+	//for i, _ := range p {
+	//	p[i] = make([]uint8, dx)
+	//	for j, _ := range p[i] {
+	//		p[i][j] = uint8((i + j) / 2)
+	//		fmt.Printf("%d %d %d\n", i, j, p[i][j])
+	//	}
+	//}
+	//
+	//return p
+
+	p := make([][]uint8, dy)
+	for i, _ := range p {
+		p[i] = make([]uint8, dx)
+		for j, _ := range p[i] {
+			p[i][j] = uint8(i * j)
+		}
+	}
+
+	return p
+
+	//p := make([][]uint8, dy)
+	//for i, _ := range p {
+	//	p[i] = make([]uint8, dx)
+	//}
+	//
+	//for i, row := range p {
+	//	for j := range row {
+	//		row[j] = uint8(i * j)
+	//	}
+	//}
+	//
+	//return p
+
+	//p := make([][]uint8, dy)
+	//for i := range p {
+	//	p[i] = make([]uint8, dx)
+	//}
+	//
+	//for y, row := range p {
+	//	for x := range row {
+	//		row[x] = uint8(x * y)
+	//	}
+	//}
+	//
+	//return p
 }
 
 func main() {
